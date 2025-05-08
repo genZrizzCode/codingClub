7bed735809909ed42b244e9ce945331f5aa1a69b
genZrizzCode <157257453+genZrizzCode@users.noreply.github.com>
2025-05-07 21:03:42 -0700

    Update max-scripts.js

--- projects/max-scripts.js
+++ projects/max-scripts.js
@@ -1,1 +1,25 @@
-/* Hi */
+(() => {
+  function getRandomString(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  }
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  const specialCharsArray = [
+    "∑", "®", "†", "¥", "π", "“", "‘", "«", "∂", "ƒ", "©", "˙", "∆", "˚", "¬",
+    "…", "Ω", "≈", "√", "∫", "≤", "≥", "÷", "¡", "™", "£", "¢", "∞", "§", "¶",
+    "•", "ª", "º", "–", "≠", "€", "‹", "›", "ﬁ", "ﬂ", "‡", "‚", "±", "„", "‰",
+    "∏", "˝", "", "¸", "˛", "◊", "ı", "¯", "¿", "大家好", "……", "(￣(工)￣)", "( ͡° ͜ʖ ͡°)"
+  ];
+
+  (async () => {
+    while (true) {
+      const randomString = getRandomString(specialCharsArray);
+      document.getElementById('special-Char-Loop').innerHTML = randomString;
+      await sleep(500); // wait 0.5 seconds
+    }
+  })();
+})();
